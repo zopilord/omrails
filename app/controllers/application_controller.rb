d3217c44d03b15531ba9a3b84dd83aabe5eb3248
Uriel Vega <urielcornel@gmail.com>
2013-04-25 13:58:42 -0500

    Agregaron usuarios y restricciones a los mismos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #rescue_from ActiveRecord::RecordNotFound, with: :user_invalid
+
+#private
+#	def user_invalid
+#		render "pages/user_invalid"
+#	end	
+	
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,5 +3,6 @@ class Pin < ActiveRecord::Base
   validates :title, uniqueness: true
   validates :title, presence: true, length: { :maximum => 10 }
   belongs_to :user
+  validates :user_id, presence: true
 end
 
